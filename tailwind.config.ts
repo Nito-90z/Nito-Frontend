e9d326b4e0c05a4b1fcf4c3ca7965b44ac9c57af
GangHub1970 <dbsrkd1970@pusan.ac.kr>
2024-10-09 10:37:10 +0900

    style : tailwind 커스텀 색상 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,11 +7,14 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+    colors: {
+      black: "#171717",
+      gray: "#aeaeae",
+      "light-gray": "#d9d9d9",
+      "dark-gray": "#333333",
+      brand: "#fc5660",
+      "light-brand": "#fef2f2",
+      green: "#4bbb86",
     },
   },
   plugins: [],
