8ed53b172ea31d4b05da95bdc2b783cfa1ecdbe1
GangHub1970 <dbsrkd1970@pusan.ac.kr>
2024-10-12 22:51:43 +0900

    refactor : 사이즈 변경을 위해 <Checkbox> 수정

--- src/components/common/CheckBox.tsx
+++ src/components/common/CheckBox.tsx
@@ -1,26 +1,39 @@
-import { useId } from 'react';
-import { twMerge } from 'tailwind-merge';
+import { useId } from "react";
+import CheckIcon from "./icons/CheckIcon";
 
-type Props = Omit<React.ComponentPropsWithoutRef<'input'>, 'type' | 'id'>;
+type Props = Omit<
+  React.ComponentPropsWithoutRef<"input">,
+  "type" | "id" | "size"
+> & {
+  size?: "sm" | "lg";
+};
 
-export default function CheckBox(props: Props) {
-  const { className, children, ...rest } = props;
+export default function Checkbox(props: Props) {
+  const { className, children, size, ...rest } = props;
   const uuid = useId();
+  const containerStyle = size === "lg" ? "w-7 h-7" : "w-5 h-5";
+  const checkStyle =
+    size === "lg" ? "top-[14px] left-[14px]" : "top-[10px] left-[10px]";
 
   return (
     <>
-      <div className='flex items-center gap-2'>
+      <label
+        htmlFor={uuid}
+        className="relative flex gap-2 w-fit cursor-pointer rounded-full"
+      >
         <input
-          type='checkbox'
+          type="checkbox"
           id={uuid}
-          className={twMerge(
-            "appearance-none w-5 h-5 border border-light-gray rounded-sm checked:bg-[url('/images/check.svg')] checked:bg-brand  checked:bg-no-repeat checked:bg-center cursor-pointer",
-            className
-          )}
+          className={`peer relative ${containerStyle} cursor-pointer appearance-none rounded-sm border border-light-gray transition-all checked:bg-brand`}
           {...rest}
         />
-        <label htmlFor={uuid}>{children}</label>
-      </div>
+        <div
+          className={`pointer-events-none absolute ${checkStyle} -translate-x-1/2 -translate-y-1/2 text-white opacity-0 transition-opacity peer-checked:opacity-100`}
+        >
+          <CheckIcon className={size === "lg" ? "w-4 h-4" : ""} />
+        </div>
+        <p className="cursor-pointer">{children}</p>
+      </label>
     </>
   );
 }
