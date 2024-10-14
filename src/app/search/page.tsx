bf87d3877505b48364d30c290e57d113234956c1
GangHub1970 <dbsrkd1970@pusan.ac.kr>
2024-10-14 15:54:40 +0900

    feat : 최근 검색어 localStorage에 저장 및 삭제 기능 구현

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,20 +1,61 @@
 "use client";
 
 import SearchResult from "@/components/search/SearchResult";
-import { useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
 
 export default function SearchPage() {
   const [keyword, setKeyword] = useState("");
+  const [recentSearches, setRecentSearches] = useState<string[]>([]);
 
+  const handleRecentSearchesDelete = (id: number) => {
+    setRecentSearches((prev) => prev.filter((_, index) => id !== index));
+  };
+  const handleRecentSearchesClear = () => {
+    setRecentSearches([]);
+    localStorage.setItem("recentSearches", JSON.stringify([]));
+  };
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const word = keyword.trim();
+    if (word === "") return;
+
+    setRecentSearches((prev) => {
+      if (prev.includes(word)) {
+        return [word, ...prev.filter((k) => k !== word)];
+      }
+      return [word, ...prev];
+    });
+    setKeyword("");
+  };
+
+  // 초기에 데이터가 비어있는 동안에 어떻게 보여줄지. 수정
+  useEffect(() => {
+    const recentSearches = localStorage.getItem("recentSearches") || "[]";
+    setRecentSearches(JSON.parse(recentSearches));
+  }, []);
+
+  useEffect(() => {
+    if (recentSearches.length === 0) return;
+
+    localStorage.setItem("recentSearches", JSON.stringify(recentSearches));
+  }, [recentSearches]);
   return (
     <section className="flex flex-col gap-5 h-full">
-      <input
-        type="text"
-        value={keyword}
-        onChange={(e) => setKeyword(e.target.value)}
-        className="h-[62px] bg-brand"
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={keyword}
+          onChange={(e) => setKeyword(e.target.value)}
+          className="w-full h-[62px] bg-brand"
+        />
+      </form>
+      <SearchResult
+        keyword={keyword}
+        recentSearches={recentSearches}
+        onDelete={handleRecentSearchesDelete}
+        onClear={handleRecentSearchesClear}
       />
-      <SearchResult keyword={keyword} />
     </section>
   );
 }
--- src/components/search/RecentSearchItem.tsx
+++ src/components/search/RecentSearchItem.tsx
@@ -1,10 +1,19 @@
 import Button from "../common/Button";
 import CloseIcon from "../common/icons/CloseIcon";
 
-export default function RecentSearchItem({ keyword }: { keyword: string }) {
+type Props = {
+  id: number;
+  keyword: string;
+  onDelete: (id: number) => void;
+};
+
+export default function RecentSearchItem({ id, keyword, onDelete }: Props) {
   return (
     <li className="py-2">
-      <Button className="flex justify-between items-center bg-transparent h-fit">
+      <Button
+        className="flex justify-between items-center bg-transparent h-fit"
+        onClick={() => onDelete(id)}
+      >
         <span className="text-dark-gray leading-7">{keyword}</span>
         <CloseIcon />
       </Button>
