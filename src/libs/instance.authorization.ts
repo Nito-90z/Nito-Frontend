af4106848891d871de9e77354a0a73731f1380fb
GangHub1970 <dbsrkd1970@pusan.ac.kr>
2024-10-16 23:07:56 +0900

    feat : access token 만료 에러 발생 시 refresh 처리
    - access token을 사용하는 API 요청을 테스트해보지 못함

--- src/libs/instance.authorization.ts
+++ src/libs/instance.authorization.ts
@@ -1,3 +1,4 @@
+import { refresh } from "@/services/user";
 import axios, { InternalAxiosRequestConfig } from "axios";
 import { cookies } from "next/headers";
 
@@ -16,3 +17,27 @@ authInstance.interceptors.request.use((config: InternalAxiosRequestConfig) => {
   }
   return config;
 });
+
+authInstance.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    if (axios.isAxiosError(error)) {
+      // accessToken이 만료된 경우
+      if (error.status === 444) {
+        const cookieStore = cookies();
+        try {
+          const originRequest = error.config as InternalAxiosRequestConfig;
+          const { accessToken, refreshToken } = await refresh();
+
+          cookieStore.set("accessToken", accessToken);
+          cookieStore.set("refreshToken", refreshToken);
+          originRequest.headers.Authorization = `Bearer ${accessToken}`;
+          return axios(originRequest);
+        } catch (error) {
+          return Promise.reject(error);
+        }
+      }
+    }
+    return Promise.reject(error);
+  }
+);
--- src/services/user.ts
+++ src/services/user.ts
@@ -1,5 +1,8 @@
 import { SignInData } from "@/fetchers/user";
 import { instance } from "@/libs/instance";
+import { authInstance } from "@/libs/instance.authorization";
+import { getCookie } from "cookies-next";
+import { cookies } from "next/headers";
 
 export async function generateNickname() {
   return instance.get("/v1/user/nickname/").then((res) => res.data);
@@ -21,4 +24,12 @@ export async function signIn(
   }
 ) {
   return instance.post("/v1/user/register/", body).then((res) => res.data);
+}
+
+export async function refresh() {
+  const refreshToken = getCookie("refreshToken", { cookies });
+
+  return instance
+    .post("/v1/user/refresh/", { refreshToken })
+    .then((res) => res.data);
 }
