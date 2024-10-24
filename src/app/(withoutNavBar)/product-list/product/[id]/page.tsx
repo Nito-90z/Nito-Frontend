f8e06340ff4392a112234a5e2acb62a054474452
GangHub1970 <dbsrkd1970@pusan.ac.kr>
2024-10-24 09:50:16 +0900

    refactor : 상품 상세 페이지 Suspense 적용
    - SSRSafeSuspense 컴포넌트를 만들어 클라이언트에서 데이터 요청 시 발생하는 서버에서의 요청을 방지 -> 444에러 해결

--- src/app/(withoutNavBar)/product-list/product/[id]/page.tsx
+++ src/app/(withoutNavBar)/product-list/product/[id]/page.tsx
@@ -1,5 +1,7 @@
 import Header from "@/components/productDetail/Header";
+import Skeleton from "@/components/productDetail/Skeleton";
 import DetailDataFetcher from "@/components/productDetail/DetailDataFetcher";
+import SSRSafeSuspense from "@/components/common/SSRSafeSuspense";
 
 type Props = {
   params: {
@@ -11,7 +13,9 @@ export default function ProductDetailPage({ params: { id } }: Props) {
   return (
     <section className="flex flex-col h-full overflow-y-auto">
       <Header />
-      <DetailDataFetcher id={id} />
+      <SSRSafeSuspense fallback={<Skeleton />}>
+        <DetailDataFetcher id={id} />
+      </SSRSafeSuspense>
     </section>
   );
 }
