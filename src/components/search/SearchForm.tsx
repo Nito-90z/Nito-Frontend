7f47ccbbea4bded2afda1c7e986d5b3a922c0e96
GangHub1970 <dbsrkd1970@pusan.ac.kr>
2024-10-28 10:06:21 +0900

    feat : 검색어 자동완성 기능 삭제

--- src/components/search/SearchForm.tsx
+++ src/components/search/SearchForm.tsx
@@ -4,35 +4,10 @@ import { useEffect, useState } from 'react';
 import SearchBar from '../header/SearchBar';
 import { twMerge } from 'tailwind-merge';
 import RecentSearch from './RecentSearch';
-// import AutoKeywords from './AutoKeywords';
 import { useProductQueryStore } from '@/stores/productQuery';
 import SearchProducts from './SearchProducts';
 import { useRecentSearchStore } from '@/stores/recentSearch';
 import { useForm } from 'react-hook-form';
-
-// // 검색어 자동 완성
-// const SEARCH_RESULTS = [
-//   '우산',
-//   '우주',
-//   '우정',
-//   '우편',
-//   '우울증',
-//   '우연히',
-//   '우체국',
-//   '초우량기업',
-//   '영웅주의',
-//   '대우조선해양',
-//   '교육과정평가원',
-//   '공무원연금공단',
-//   '우주항공산업',
-//   '우주비행사',
-//   '우편배달부',
-//   '두뇌회전속도',
-//   '무궁화호열차',
-//   '기후변화적응',
-//   '국제우주정거장',
-//   '세계자연유산위원회',
-// ];
 
 export type SearchKeyword = { keyword: string };
 
@@ -83,19 +58,13 @@ export default function SearchForm() {
         setFocus={() => setFocus('keyword')}
         setIsSearchBarFocus={setIsSearchBarFocus}
       />
-      {
-        isSearchBarFocus && (
-          // (watch('keyword') ? (
-          //   <AutoKeywords results={SEARCH_RESULTS} />
-          // ) : (
-          <RecentSearch
-            setKeyword={handleRecentSearchClick}
-            onDelete={handleRecentSearchesDelete}
-            onClear={handleRecentSearchesClear}
-          />
-        )
-        // ))
-      }
+      {isSearchBarFocus && (
+        <RecentSearch
+          setKeyword={handleRecentSearchClick}
+          onDelete={handleRecentSearchesDelete}
+          onClear={handleRecentSearchesClear}
+        />
+      )}
       <SearchProducts />
     </section>
   );
