1a97446ed515b63ed2b962c42ff56229a2527d79
GangHub1970 <dbsrkd1970@pusan.ac.kr>
2024-10-29 16:44:32 +0900

    fix : toast 여러번 호출 시 시간이 중첩되는 문제 해결

--- src/stores/toast.ts
+++ src/stores/toast.ts
@@ -6,12 +6,18 @@ type ToastState = {
   setToast: (text: string | null, time?: number) => void;
 };
 
+let timer: number | null = null;
+
 const toastStore = create<ToastState>()((set) => ({
   text: null,
   setToast: (text: string | null, time?: number) => {
     set(() => ({ text }));
-    if (text !== null && time) {
-      setTimeout(() => {
+
+    if (text) {
+      if (timer) {
+        window.clearTimeout(timer);
+      }
+      timer = window.setTimeout(() => {
         set(() => ({ text: null }));
       }, time);
     }
